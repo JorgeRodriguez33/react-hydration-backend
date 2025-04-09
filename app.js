0888d6655a4ec7239bbf6595be1706f4abadc771
JorgeRodriguez33 <jorgero33@gmail.com>
2025-04-08 23:42:20 -0300

    agregando

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const path = require("path");
 
+
+const cors = require('cors'); 
+
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public"))); // Servir archivos estáticos
 
+app.use(cors());
 
 const initialPlaces = [
   { label: "Plaza Independencia", coordinates: [-34.9055, -56.1860] },
